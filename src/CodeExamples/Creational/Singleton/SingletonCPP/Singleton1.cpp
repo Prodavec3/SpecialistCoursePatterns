1b10564af9abad527728eb7ff91616fb6b5f704e
Prodavec3 <destroedst@mail.ru>
2024-05-22 22:14:08 +0300

    rebuild code expl

--- src/CodeExamples/Creational/Singleton/SingletonCPP/Singleton1.cpp
+++
@@ -1,9 +0,0 @@
-#include "Singleton1.h"
-
-Singleton1* Singleton1::_instance = 0;
-Singleton1* Singleton1::Instance() {
-  if(_instance == 0){
-     _instance = new Singleton1;
-  }
-  return _instance;
-}
