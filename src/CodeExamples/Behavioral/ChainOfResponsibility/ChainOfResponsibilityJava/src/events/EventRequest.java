7adeb9ced2e159facb946ba17e341b9cdce179f6
Prodavec3 <destroedst@mail.ru>
2024-05-22 15:39:30 +0300

    add

---
+++ src/CodeExamples/Behavioral/ChainOfResponsibility/ChainOfResponsibilityJava/src/events/EventRequest.java
@@ -0,0 +1,16 @@
+package events;
+
+public class EventRequest {
+	private boolean handled = false;
+
+	public boolean isHandled() {
+		return handled;
+	}
+
+	public void setHandled(boolean handled) {
+		this.handled = handled;
+	}
+	
+	
+	
+}
---
+++ src/CodeExamples/Behavioral/ChainOfResponsibility/ChainOfResponsibilityJava/src/events/Handler.java
@@ -0,0 +1,7 @@
+package events;
+
+public interface Handler {
+
+	void setNextHandler(Handler next);
+	void handle(EventRequest request);
+}
